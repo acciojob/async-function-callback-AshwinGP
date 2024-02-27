29d9582df7d708790ea5916b9588d29361500845
AshwinGP <111189173+AshwinGP@users.noreply.github.com>
2024-02-27 18:09:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+var btn=document.getElementById("btn")
 
+
+async function wait(){
+	// const url = "https://jsonplaceholder.typicode.com/posts/1";
+	var res=await fetch(url)
+	let json=await res.json();
+  return document.getElementById("output").innerHTML=json.title;  
+} 
+
+
+
+
+btn.addEventListener('click',()=>{
+    wait()
+})
 //your JS code here. If required.
